b9187317ac4c390709c58b4e27deb9f1da5d1cb4
Timo Noordzee <timo_noordzee@hotmail.com>
2022-12-02 21:03:05 +0100

    Day02

---
+++ src/Day02.kt
@@ -0,0 +1,32 @@
+import kotlin.system.measureTimeMillis
+
+private operator fun String.component1() = this[0]
+private operator fun String.component2() = this[1]
+private operator fun String.component3() = this[2]
+
+fun main() {
+
+    fun part1(input: List<String>) = input.sumOf { (theirShape, _, yourShape) ->
+        val shapeScore = yourShape - 'W'
+        val outcome = (yourShape - theirShape - 1) % 3 // 0 = lost, 1 = draw, 2 = won
+        shapeScore + outcome * 3
+    }
+
+    fun part2(input: List<String>) = input.sumOf { (theirShape, _, outcome) ->
+        val resultScore = (outcome - 'X') * 3
+        val shapeScore = (theirShape - 'A' + (outcome - 'V')) % 3 + 1
+        resultScore + shapeScore
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day02_test")
+    check(part1(testInput) == 15)
+    check(part2(testInput) == 12)
+
+    val time = measureTimeMillis {
+        val input = readInput("Day02")
+        println(part1(input))
+        println(part2(input))
+    }
+    println("finished in ${time}ms")
+}
